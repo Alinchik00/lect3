29eb230113538005a0116123f6117773a58ba139
Alina <kan.alina2017@yandex.ru>
2022-11-13 20:23:48 +0300

    задача 4й тип функций

---
+++ ex5correct/Program.cs
@@ -0,0 +1,14 @@
+﻿string GetRange(int quarter)
+{
+    if (quarter==1) return "x>0 & y>0";
+    if (quarter==2) return "x<0 & y>0";
+    if (quarter==3) return "x<0 & y<0";
+    if (quarter==4) return "x>0 & y<0";
+
+    return "unknown quarter";
+}
+
+Console.WriteLine("Enter the Quarter number:");
+int quarterNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetRange(quarterNumber));
