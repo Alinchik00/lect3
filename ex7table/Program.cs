dbc2c594691d040a4714824813b91eaef6c324b9
Alina <kan.alina2017@yandex.ru>
2022-11-13 23:48:21 +0300

    таблица квадратов

---
+++ ex7table/Program.cs
@@ -0,0 +1,9 @@
+﻿// принимаем число н, получаем таблицу квадратов от 1 до н. Например 3: 1, 4, 9
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i<=n; i++)
+{
+    double result = Math.Pow(i,2);
+    Console.WriteLine(result);
+}
