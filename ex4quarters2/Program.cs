42884076052e83af0401f75c2acc61b7d7f92b76
Alina <kan.alina2017@yandex.ru>
2022-11-13 19:18:03 +0300

    задача на четверти функции

---
+++ ex4quarters2/Program.cs
@@ -0,0 +1,32 @@
+﻿int GetQuarterNumber(int x, int y)
+{
+    Console.WriteLine("GetQuarterNumber: " + x + " " + y);
+
+    if (x == 0 | y == 0)
+    return -1;
+
+    if (x>0 && y>0)
+    return 1;
+
+    if (x<0 && y>0)
+    return 2;
+
+    if (x<0 && y<0)
+    return 3;
+
+    if (x>0 && y<0)
+    return 4;
+}
+
+Console.WriteLine("Введите Х:");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите У:");
+int y = int.Parse(Console.ReadLine()!);
+
+int result = GetQuarterNumber(x, y);
+
+if (result>0)
+Console.WriteLine("Номер четверти, в котором лежит точка: " + result);
+else
+Console.WriteLine("Неверные вводные данные");
