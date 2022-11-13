31364d99eb0de08c7aacf100c5e505598af87280
Alina <kan.alina2017@yandex.ru>
2022-11-13 22:15:11 +0300

    расстояние между точками

---
+++ ex6coordinates/Program.cs
@@ -0,0 +1,22 @@
+﻿// принимает координаты 2х точек, находит между ними расстояние
+double distance (int[] dotsA, int[] dotsB)
+{
+    return Math.Sqrt(Math.Pow(dotsA[0]-dotsB[0], 2)+Math.Pow(dotsA[1]-dotsB[1], 2));
+}
+
+int[] dotsA = new int[2];
+int[] dotsB = new int[2];
+
+Console.WriteLine("Enter the X(A):");
+dotsA[0] = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter the Y(A):");
+dotsA[1] = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter the X(B):");
+dotsA[0] = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter the Y(B):");
+dotsA[1] = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Math.Round(distance(dotsA, dotsB),2));
