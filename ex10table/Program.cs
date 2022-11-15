db86c678df4d8c02a44fbdc1d67ec2f39a9ba48c
Alina <kan.alina2017@yandex.ru>
2022-11-16 00:11:42 +0300

    задача таблица кубов

---
+++ ex10table/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+// 3 -> 1, 8, 27
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i<=n; i++)
+{
+    double result = Math.Pow(i,3);
+    Console.WriteLine(result);
+}
