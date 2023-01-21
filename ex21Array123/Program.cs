1724b4343733d7d0930680d498b9aa5036420e5f
Alina <kan.alina2017@yandex.ru>
2023-01-21 23:39:31 +0300

    массив из 123 чисел

---
+++ ex21Array123/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("");
+
+Console.WriteLine("Введите длину массива");
+int length = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[length];
+
+void RandomArray(int[] array) // Функция заполнения массива в случайном порядке
+{
+    Random random = new Random();
+    for (int i= 0; i < length; i++)
+{
+    array[i] = random.Next(0, 123);
+}
+}
+
+void PrintArray(int[] array) // Функция вывода массива в консоль
+{
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine(array[i] + " ");
+    }
+}
+
+RandomArray(array);
+Console.Write("Массив в строку ");
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < length; i++)
+{
+    if (array[i] > 10 && array[i] < 100)
+    count++;
+}
+Console.WriteLine("Количество 2ч значных чисел: " + count);
